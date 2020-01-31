94137927c5162a80fc0856a029d654f491b8b70f
Orkhan Farmanli <orkhanfarmanli@gmail.com>
2020-01-31 17:36:42 +0400

    Initial version is ready, need refactoring and redesigning

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    private $common_words = [
+        'the', 'be', 'to', 'of', 'and', 'a', 'in', 'that', 'have', 'I', 'it', 'for', 'not', 'on', 'with', 'he', 'as', 'you', 'do', 'at', 'this', 'but', 'his', 'by', 'from', 'they', 'we', 'say', 'her', 'she', 'or', 'an', 'will', 'my', 'one', 'all', 'would', 'there', 'their', 'what', 'so', 'up', 'out', 'if', 'about', 'who', 'get', 'which', 'go', 'me,'
+    ];
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $rss_feed_url = "https://www.theregister.co.uk/software/headlines.atom";
+        $xml = file_get_contents($rss_feed_url);
+        $feed = simplexml_load_string($xml);
+        $most_frequent_words = $this->findMostFrequentWords($feed);
+
+        return view('home', ['feed' => $feed, 'most_frequent_words' => $most_frequent_words]);
+    }
+
+    /**
+     * Find most frequent words in given string
+     * @return array
+     */
+    private function findMostFrequentWords($feed)
+    {
+        $words = [];
+
+        foreach ($feed->children() as $post) {
+            if ($post->title && $post->summary) {
+                $stripped_title = strip_tags($post->title);
+                $stripped_summary = strip_tags($post->summary);
+
+                $found_words = str_word_count($stripped_title . ' ' . $stripped_summary, 1);
+                $words = array_merge_recursive($words, $found_words);
+            }
+        }
+
+        $most_frequent_words = array_count_values($words);
+
+        foreach ($most_frequent_words as $key => $value) {
+            if (in_array($key, $this->common_words)) {
+                unset($most_frequent_words[$key]);
+            }
+        }
+
+        arsort($most_frequent_words);
+
+        return array_slice($most_frequent_words, 0, 10);
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-9">
+            <div class="card">
+                <div class="card-header">RSS feed</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
+                    @endif
+
+                    @foreach ($feed->children() as $post)
+                    @if ($post->title && $post->summary)
+                    <div>
+                        <span>{{ $post->author }}</span>
+                        <h3><b>{!! $post->title !!}</b></h3>
+                        {!! $post->summary !!}
+                        <b>
+                            author: <a href="{{ $post->author->uri }}" target="_blank">{{ $post->author->name }}</a>
+                        </b>
+                        <a class="float-right" href="{{ $post->link->attributes()->href }}" target="_blank">
+                            read more
+                        </a>
+                    </div>
+                    <hr>
+                    @endif
+                    @endforeach
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-3">
+            <div class="card">
+                <div class="card-header">Most frequent words</div>
+                <div class="card-body">
+                    @php
+                    $line = 1;
+                    @endphp
+                    @foreach ($most_frequent_words as $key => $value)
+                    <p>{{ $line++ . '. ' . $key . " ($value)" }}</p>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
